027ed2b103ecb0f37925e14ea796a5fc329ec452
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-16 01:13:03 -0400

    chore: fix typo on sic module prefix

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ if "google.cloud" in packages:
 
 setuptools.setup(
     name=name,
-    version=setuptools.sic(version),
+    version=sic(version),
     description=description,
     long_description=readme,
     author="Google LLC",
